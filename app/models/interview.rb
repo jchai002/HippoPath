b96152829f38ef08b57247a2062a730859a182ae
Jerry Chai <jchai002@gmail.com>
2016-01-03 23:01:05 -0800

    add associations

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -1,2 +1,4 @@
 class Interview < ActiveRecord::Base
+  belongs_to :student
+  belongs_to :hospital
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,5 @@
 class Student < ActiveRecord::Base
+  has_many :addresses
+  has_many :interviews
+  belongs_to :school
 end
