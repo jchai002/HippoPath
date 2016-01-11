9c076b0e86cc92cb9bc9ebd92d8b2532526260ad
Jerry Chai <jchai002@gmail.com>
2016-01-10 23:42:16 -0800

    add devise forms

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,13 @@ class User < ActiveRecord::Base
   has_many :interviews
   belongs_to :school
   belongs_to :address
-  
+
   TEMP_EMAIL_PREFIX = 'change@me'
   TEMP_EMAIL_REGEX = /\Achange@me/
 
   # Include default devise modules. Others available are:
-  # :lockable, :timeoutable
-  devise :database_authenticatable, :registerable, :confirmable,
-    :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+  # :lockable, :timeoutable, :confirmable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
   validates_format_of :email, :without => TEMP_EMAIL_REGEX, on: :update
 
