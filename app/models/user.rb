8e69f8a2d8297192523da63887ffc433a9cbeef3
Jerry Chai <jchai002@gmail.com>
2016-01-11 00:58:26 -0800

    able to login with facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,8 +29,9 @@ class User < ActiveRecord::Base
       # Get the existing user by email if the provider gives us a verified email.
       # If no verified email was provided we assign a temporary email and ask the
       # user to verify it on the next step via UsersController.finish_signup
-      email_is_verified = auth.info.email && (auth.info.verified || auth.info.verified_email)
-      email = auth.info.email if email_is_verified
+      #email_is_verified = auth.info.email && (auth.info.verified || auth.info.verified_email)
+      binding.pry
+      email = auth.info.email #if email_is_verified
       user = User.where(:email => email).first if email
 
       # Create the user if it's a new registration
@@ -41,7 +42,6 @@ class User < ActiveRecord::Base
           email: email ? email : "#{TEMP_EMAIL_PREFIX}-#{auth.uid}-#{auth.provider}.com",
           password: Devise.friendly_token[0,20]
         )
-        user.skip_confirmation!
         user.save!
       end
     end
