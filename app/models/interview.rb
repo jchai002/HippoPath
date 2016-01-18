3621ce4039407632a8bdde534293153a80c6bd79
Jerry Chai <jchai002@gmail.com>
2016-01-17 16:34:09 -0800

    add search method for interviews

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -1,4 +1,14 @@
 class Interview < ActiveRecord::Base
   belongs_to :user
   belongs_to :hospital
+
+  #takes a search object with properties
+  def self.search(search)
+    if search
+      where('hospital LIKE ? AND date LIKE ? AND ride_status LIKE ?', "%#{search.hosptial.upcase}%","%#{search.date.upcase}%","%#{search.ride_status.upcase}%")
+    else
+      all
+    end
+  end
+
 end
