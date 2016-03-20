f5635d866a416d76da69be31c06abb502e7386f8
Jerry Chai <jchai002@gmail.com>
2016-03-20 00:24:13 -0700

    modify seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,15 @@
-h1 = Hospital.create({name:"hospital 1"})
-h2 = Hospital.create({name:"hospital 2"})
+hospitals = []
+(1..10).each do |number|
+  Hospital.create({name:"Hospital #{number}"})
+end
+
 u1= User.create({name:"jerry", email:'jerry@gdd.com', password:'12345678'})
 u2= User.create({name:"chai", email:'chai@gdd.com', password:'12345678'})
 u3= User.create({name:"dog", email:'dog@gdd.com', password:'12345678'})
+users = [u1,u2,u3]
 
-5.times do
-  Interview.create({date: "today",time:"8:00AM", ride_status:"Need Ride", hospital:h1})
+users.each do |user|
+  5.times do
+    user.interviews.create({date: "today",time:"8:00AM", ride_status:"Need Ride", hospital:hospitals.first})
+  end
 end
