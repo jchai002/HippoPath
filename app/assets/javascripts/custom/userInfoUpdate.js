8ee4a5d4c7e53cd716b09781cf5e37808193a74a
Jerry Chai <jchai002@gmail.com>
2016-03-27 13:43:50 -0700

    add frontend validation for password

--- app/assets/javascripts/custom/userInfoUpdate.js
+++ app/assets/javascripts/custom/userInfoUpdate.js
@@ -1,3 +1,31 @@
+$(document).ready(function(){
+  if ($('#user-info')[0]) {
+    $('.info-edit').submit(function(e){
+      e.preventDefault();
+    })
+    updateUserInfo($('#email-display'),$('#email-edit-field'),$('#email-edit-handle'), 'email');
+    updateUserInfo($('#school-display'),$('#school-edit-field'),$('#school-edit-handle'), 'school');
+    updateUserInfo($('#specialty-display'),$('#specialty-edit-field'),$('#specialty-edit-handle'), 'specialty');
+    $('#gender-select').change(function(){
+      userId = $('#user-info').data('userid')
+      var jsonData = {'user': {}};
+      jsonData['user']['gender'] = $(this).val();
+      $.ajax({
+        url: "/users/" + userId,
+        dataType: 'json',
+        type: 'PUT',
+        data: jsonData,
+        success: function(data) {
+          console.log('gender change success')
+        },
+        error: function(xhr, status, err) {
+          console.error( status, err.toString());
+        }
+      });
+    })
+  }
+});
+
 function updateUserInfo ($display,$editField,$handle, paramName) {
   userId = $('#user-info').data('userid')
   $handle.click(function(){
@@ -38,31 +66,3 @@ function updateUserInfo ($display,$editField,$handle, paramName) {
     });
   });
 }
-
-$(document).ready(function(){
-  if ($('#user-info')[0]) {
-    $('.info-edit').submit(function(e){
-      e.preventDefault();
-    })
-    updateUserInfo($('#email-display'),$('#email-edit-field'),$('#email-edit-handle'), 'email');
-    updateUserInfo($('#school-display'),$('#school-edit-field'),$('#school-edit-handle'), 'school');
-    updateUserInfo($('#specialty-display'),$('#specialty-edit-field'),$('#specialty-edit-handle'), 'specialty');
-    $('#gender-select').change(function(){
-      userId = $('#user-info').data('userid')
-      var jsonData = {'user': {}};
-      jsonData['user']['gender'] = $(this).val();
-      $.ajax({
-        url: "/users/" + userId,
-        dataType: 'json',
-        type: 'PUT',
-        data: jsonData,
-        success: function(data) {
-          console.log('gender change success')
-        },
-        error: function(xhr, status, err) {
-          console.error( status, err.toString());
-        }
-      });
-    })
-  }
-});
