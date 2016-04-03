24f42793f6952c1825a7f152d6ecc9688a57f39d
Jerry Chai <jchai002@gmail.com>
2016-04-02 19:44:53 -0700

    style conversation dropdown

--- app/assets/javascripts/custom/conversationList.js
+++ app/assets/javascripts/custom/conversationList.js
@@ -1,16 +1,16 @@
 var ready = function () {
   $.get("/conversations", function (data) {
-    $('.conversation-list').html(data);
+    $('.conversation-container').html(data);
   }, "html");
 
-  $('#conversation-list-toggle').click(function(){
+  $('#conversation-container-toggle').click(function(){
     var openState = $(this).data('open-state');
     if (openState) {
-      $('.conversation-list')
+      $('.conversation-container')
         .slideUp(500);
       $(this).data('open-state', false);
     } else {
-      $('.conversation-list')
+      $('.conversation-container')
         .slideDown(500);
       $(this).data('open-state', true);
     }
