884c4528a5be84e6bb18359c3667908b1f574873
Jerry Chai <jchai002@gmail.com>
2016-04-03 22:21:04 -0700

    add paperclip and omniauth profile picture upload

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160313054153) do
+ActiveRecord::Schema.define(version: 20160404042620) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,8 +30,6 @@ ActiveRecord::Schema.define(version: 20160313054153) do
   create_table "conversations", force: :cascade do |t|
     t.integer  "sender_id"
     t.integer  "recipient_id"
-    t.integer  "sender_chatbox_state"
-    t.integer  "recipient_chatbox_state"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -121,6 +119,10 @@ ActiveRecord::Schema.define(version: 20160313054153) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "name"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
