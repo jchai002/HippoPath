09d45c05911f2064a72992990496bae921dd2f8d
Jerry Chai <jerry@gooddogdesign.com>
2016-04-05 10:24:13 -0700

    home page styling

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,9 @@
 class HomeController < ApplicationController
+
   def index
     @student_count=User.all.count
     @school_count= School.all.count
     @hospital_count= Hospital.all.count
+    @landing_page = true
   end
 end
