1143f2c974f3d3a1f6530ea2a438ee016bf0a607
Jerry Chai <jchai002@gmail.com>
2016-04-06 22:18:25 -0700

    delete deprecated components

--- app/assets/javascripts/components/search/searchResultsPanel.js.jsx
+++
@@ -1,9 +0,0 @@
-var SearchResultsPanel = React.createClass({
-  render: function() {
-    return (
-    <div className="panel panel-default">
-      <SearchResultsTable data={this.props.data} token={this.props.token} currentUserId={this.props.currentUserId}/>
-    </div>
-);
-}
-});
