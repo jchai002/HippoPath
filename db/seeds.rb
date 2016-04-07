636880aa94dfb57032e9f9b65c7e8a64070e60dc
Jerry Chai <jchai002@gmail.com>
2016-04-07 01:01:09 -0700

    modify seed file to increase number of users

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,14 @@ end
 u1= User.create({name:"jerry", email:'jerry@gdd.com', password:'12345678'})
 u2= User.create({name:"chai", email:'chai@gdd.com', password:'12345678'})
 u3= User.create({name:"dog", email:'dog@gdd.com', password:'12345678'})
-users = [u1,u2,u3]
+u4= User.create({name:"tea", email:'tea@gdd.com', password:'12345678'})
+u5= User.create({name:"is", email:'is@gdd.com', password:'12345678'})
+u6= User.create({name:"super", email:'super@gdd.com', password:'12345678'})
+u7= User.create({name:"awesome", email:'awesome@gdd.com', password:'12345678'})
+u8= User.create({name:"okay", email:'okay@gdd.com', password:'12345678'})
+u9= User.create({name:"bai", email:'bai@gdd.com', password:'12345678'})
+u10= User.create({name:"yah", email:'yah@gdd.com', password:'12345678'})
+users = [u1,u2,u3,u4,u5,u6,u7,u8,u9,u10]
 
 users.each do |user|
   5.times do
