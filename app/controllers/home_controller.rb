d0986265f2e23afe786ee202fbb0d44a694fff72
Jerry Chai <jchai002@gmail.com>
2016-04-08 19:54:08 -0700

    style login forms

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,5 @@ class HomeController < ApplicationController
     @student_count=User.all.count
     @school_count= School.all.count
     @hospital_count= Hospital.all.count
-    @landing_page = true
   end
 end
