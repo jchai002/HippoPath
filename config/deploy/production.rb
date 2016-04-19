68cbdc6ddf6b4a5a6f7ad518688851c39593d8e3
Jerry Chai <jchai002@gmail.com>
2016-04-19 00:46:05 -0700

    update ec2 server ip

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server '172.31.33.63', user: 'deploy', roles: %w{web app db}
+server '54.191.133.42', user: 'deploy', roles: %w{web app db}
 
 # server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
