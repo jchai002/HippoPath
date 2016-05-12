48b9fe63a5e56b53e4906139d5b894563a4e5ad9
Jerry Chai <jerry@gooddogdesign.com>
2016-05-12 13:58:41 -0700

    add environment variable based logic to faye initializer

--- config/initializers/faye_start.rb
+++ config/initializers/faye_start.rb
@@ -1,3 +1,5 @@
-Thread.new do
-  system("RAILS_ENV=production bundle exec rackup private_pub.ru -s thin -E production -p 9292")
+unless ENV['NOSOCK']
+  Thread.new do
+    system("RAILS_ENV=production bundle exec rackup private_pub.ru -s thin -E production -p 9292")
+  end
 end
