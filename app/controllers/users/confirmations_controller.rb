4fcca4114ff619139e2b77b9b64a0e7674ed916f
Jerry Chai <jerry@gooddogdesign.com>
2016-05-24 15:16:11 -0700

    resend confirmation email pages WIP

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -1,12 +1,7 @@
 class Users::ConfirmationsController < Devise::ConfirmationsController
-  # GET /resource/confirmation/new
-  def new
-    binding.pry
-  end
-
   # The path used after resending confirmation instructions.
   def after_resending_confirmation_instructions_path_for(resource_name)
-    flash[:notice] = "A confirmation email has been sent to #{resource.email}"
+    flash[:notice] = "A new confirmation email has been sent to #{resource.email}"
     return new_user_session_path
   end
 end
