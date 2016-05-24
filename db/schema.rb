2e6a998983c6a509ca861e415b82fd7ae0ee7abd
Jerry Chai <jerry@gooddogdesign.com>
2016-05-24 12:12:26 -0700

    able to send signup confirmation emails

--- db/schema.rb
+++ db/schema.rb
@@ -107,6 +107,10 @@ ActiveRecord::Schema.define(version: 20160404042620) do
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
     t.string   "gender"
     t.string   "phone"
     t.string   "specialty"
@@ -123,6 +127,7 @@ ActiveRecord::Schema.define(version: 20160404042620) do
     t.datetime "image_updated_at"
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
